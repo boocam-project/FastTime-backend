4d8d83c3d3c63546ee5ea9e8fd9edec97c9749e6
최혜미 <ghrltjdtprbs@gmail.com>
2023-08-10 18:14:19 +0900

    Add files via upload

---
+++ src/main/java/com/fasttime/FastTimeApplication.java
@@ -0,0 +1,13 @@
+package com.fasttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fasttime/FastTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
