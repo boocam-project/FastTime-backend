e570b0f7798edebd15c1b50a9a320512a0d18034
hym <ghrltjdtprbs@gmail.com>
2023-08-22 00:33:57 +0900

    [style] 코드 정렬

--- src/main/java/com/fasttime/domain/member/exception/UserNotFoundException.java
+++ src/main/java/com/fasttime/domain/member/exception/UserNotFoundException.java
@@ -1,7 +1,8 @@
 package com.fasttime.domain.member.exception;
 
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException(String message){
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) {
         super(message);
     }
 }
--- src/main/java/com/fasttime/domain/member/service/EmailService.java
+++ src/main/java/com/fasttime/domain/member/service/EmailService.java
@@ -1,4 +1,5 @@
 package com.fasttime.domain.member.service;
+
 import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class EmailService {
+
     private final JavaMailSender emailsender; // Bean 등록해둔 MailConfig 를 emailsender 라는 이름으로 autowired
     private String authNum; // 인증번호
 
@@ -22,15 +24,15 @@ public class EmailService {
         Random random = new Random();
         StringBuffer key = new StringBuffer();
 
-        for(int i=0;i<8;i++) {
+        for (int i = 0; i < 8; i++) {
             int index = random.nextInt(3);
 
             switch (index) {
-                case 0 :
-                    key.append((char) ((int)random.nextInt(26) + 97));
+                case 0:
+                    key.append((char) ((int) random.nextInt(26) + 97));
                     break;
                 case 1:
-                    key.append((char) ((int)random.nextInt(26) + 65));
+                    key.append((char) ((int) random.nextInt(26) + 65));
                     break;
                 case 2:
                     key.append(random.nextInt(9));
@@ -41,7 +43,8 @@ public class EmailService {
     }
 
     // 메일 양식 작성
-    public MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+    public MimeMessage createMessage(String to)
+        throws MessagingException, UnsupportedEncodingException {
         createCode();
         String setFrom = "gjwldud0719@naver.com";
         String toEmail = to;
@@ -53,8 +56,8 @@ public class EmailService {
         message.setFrom(setFrom);
         String n = "";
         n += "<div style='margin:100px;'>";
-        n +="<h1> 안녕하세요 FastTime 입니다.</h1>";
-        n +="<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요.<p>";
+        n += "<h1> 안녕하세요 FastTime 입니다.</h1>";
+        n += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요.<p>";
         n += "<br>";
         n += "<div align='center' style='border:1px solid black; font-family:verdana';>";
         n += "<h3 style='color:#9AC1D1;'>회원가입 인증 코드</h3>";
@@ -63,18 +66,18 @@ public class EmailService {
         n += authNum + "</strong><div><br/> "; // 메일에 인증번호 넣기
         n += "</div>";
 
-        message.setText(n,"utf-8", "html");
+        message.setText(n, "utf-8", "html");
         return message;
     }
 
 
     // 메일 발송
-    public String sendSimpleMessage(String to) throws Exception{
+    public String sendSimpleMessage(String to) throws Exception {
         createCode(); // 랜덤 인증코드 생성
         MimeMessage message = createMessage(to); // 메일 발송
-        try{
+        try {
             emailsender.send((message));
-        } catch(MailException es){
+        } catch (MailException es) {
             es.printStackTrace();
             throw new IllegalArgumentException();
         }
