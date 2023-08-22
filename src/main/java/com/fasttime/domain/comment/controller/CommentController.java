381901c4226da6e29357b17737cda4f8265bdf33
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2023-08-22 14:33:02 +0900

    [refactor] ModelAndView 대신 Model을 사용하도록 수정

--- src/main/java/com/fasttime/domain/comment/controller/CommentController.java
+++ src/main/java/com/fasttime/domain/comment/controller/CommentController.java
@@ -9,9 +9,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
@@ -23,32 +25,29 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/create")
-    public ModelAndView create(@Valid @RequestBody CreateCommentRequest createCommentRequest,
-        ModelAndView mv) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@Valid @RequestBody CreateCommentRequest createCommentRequest,
+        Model model) {
         log.info("CreateCommentRequest: " + createCommentRequest);
-        mv.setStatus(HttpStatus.CREATED);
-        mv.addObject(commentService.createComment(createCommentRequest));
-        mv.setViewName("registerCommentSuccess");
-        return mv;
+        model.addAttribute(commentService.createComment(createCommentRequest));
+        return "registerCommentSuccess";
     }
 
     @PostMapping("/delete")
-    public ModelAndView delete(@Valid @RequestBody DeleteCommentRequest deleteCommentRequest,
-        ModelAndView mv) {
+    @ResponseStatus(HttpStatus.OK)
+    public String delete(@Valid @RequestBody DeleteCommentRequest deleteCommentRequest,
+        Model model) {
         log.info("DeleteCommentRequest: " + deleteCommentRequest);
-        mv.setStatus(HttpStatus.OK);
-        mv.addObject(commentService.deleteComment(deleteCommentRequest));
-        mv.setViewName("removeCommentSuccess");
-        return mv;
+        model.addAttribute(commentService.deleteComment(deleteCommentRequest));
+        return "removeCommentSuccess";
     }
 
     @PostMapping("/update")
-    public ModelAndView update(@Valid @RequestBody UpdateCommentRequest updateCommentRequest,
-        ModelAndView mv) {
+    @ResponseStatus(HttpStatus.OK)
+    public String update(@Valid @RequestBody UpdateCommentRequest updateCommentRequest,
+        Model model) {
         log.info("UpdateCommentRequest: " + updateCommentRequest);
-        mv.setStatus(HttpStatus.OK);
-        mv.addObject(commentService.updateComment(updateCommentRequest));
-        mv.setViewName("updateCommentSuccess");
-        return mv;
+        model.addAttribute(commentService.updateComment(updateCommentRequest));
+        return "updateCommentSuccess";
     }
 }
