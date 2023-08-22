27488aa4541c7ab3989c1156872116a2c0cd03fc
최혜미 <ghrltjdtprbs@gmail.com>
2023-08-22 14:17:09 +0900

    [feat] MemberRepository타입변경
    Integer -> Long

--- src/main/java/com/fasttime/domain/member/repository/MemberRepository.java
+++ src/main/java/com/fasttime/domain/member/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
     Optional<Member> findByNickname(String nickname);
