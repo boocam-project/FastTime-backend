56bac4b04ed7b46edb876ba8165c1f0ef4aff954
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-22 20:35:44 +0900

    [feat] findByEmail 메소드 추가

--- src/main/java/com/fasttime/domain/member/repository/MemberRepository.java
+++ src/main/java/com/fasttime/domain/member/repository/MemberRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>{
 
 
     Optional<Member> findByNickname(String nickname);
 
+    Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
