dd1637a6615e9fd744bf1fef21a204759a0a88b1
Nine-JH <rnwkgus2@gmail.com>
2023-08-23 17:55:17 +0900

    [feature] 게시글을 삭제할 수 있다.
    - 게시글 삭제 기능 추가.

--- src/main/java/com/fasttime/domain/post/service/PostCommandService.java
+++ src/main/java/com/fasttime/domain/post/service/PostCommandService.java
@@ -1,6 +1,7 @@
 package com.fasttime.domain.post.service;
 
 import com.fasttime.domain.post.dto.service.request.PostCreateServiceDto;
+import com.fasttime.domain.post.dto.service.request.PostDeleteServiceDto;
 import com.fasttime.domain.post.dto.service.request.PostUpdateServiceDto;
 import com.fasttime.domain.post.dto.service.response.PostResponseDto;
 import com.fasttime.domain.post.entity.Post;
@@ -38,6 +39,16 @@ public class PostCommandService {
         post.update(serviceDto.getContent());
 
         return PostResponseDto.of(post);
+    }
+
+    public void deletePost(PostDeleteServiceDto serviceDto) {
+
+        Post post = postRepository.findById(serviceDto.getPostId())
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+
+        validateMemberAuthority(serviceDto.getMemberId(), post.getMember().getId());
+
+        post.delete(serviceDto.getDeletedAt());
     }
 
     private static void validateMemberAuthority(Long requesterId, Long writerId) {
