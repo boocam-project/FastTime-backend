88c03569b9f9aa747c751e80548cfc2cdc4af5e7
Nine-JH <rnwkgus2@gmail.com>
2023-08-23 17:16:41 +0900

    [feature] 게시글 신고를 할 수 있다. #22
    - 게시글 신고 기능추가
    - 신고 허용 및 거절 가능
    - 삭제 로직 추가
    
    ISSUE #22

--- src/test/java/com/fasttime/domain/post/unit/entity/PostTest.java
+++ src/test/java/com/fasttime/domain/post/unit/entity/PostTest.java
@@ -3,8 +3,13 @@ package com.fasttime.domain.post.unit.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasttime.domain.post.entity.Post;
+import com.fasttime.domain.post.entity.ReportStatus;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class PostTest {
 
@@ -40,5 +45,179 @@ class PostTest {
         // then
         assertThat(createdPost).extracting("title", "content", "anonymity")
             .containsExactly(title, updateContent, anounumity);
+    }
+
+
+    @DisplayName("report()는")
+    @Nested
+    class Context_report{
+
+        @DisplayName("ReportStatus를 REPORTED로 변경할 수 있다.")
+        @Test
+        void reportStatus_isNotReported_willChangedToReportPending(){
+            // given
+            Post testPost = Post.builder()
+                .reportStatus(ReportStatus.NORMAL)
+                .build();
+
+            // when
+            testPost.report();
+
+            // then
+            assertThat(testPost.getReportStatus()).isSameAs(ReportStatus.REPORTED);
+        }
+
+        @DisplayName("이미 신고당한 상태면 ReportStatus를 변경할 수 없다.")
+        @EnumSource(value = ReportStatus.class, names = {"REPORTED", "REPORT_REJECTED"})
+        @ParameterizedTest
+        void reportStatus_alreadyReported_willNotChanged(ReportStatus reportStatus){
+            // given
+            Post testPost = Post.builder()
+                .reportStatus(reportStatus)
+                .build();
+
+            // when
+            testPost.report();
+
+            // then
+            assertThat(testPost.getReportStatus()).isSameAs(reportStatus);
+        }
+    }
+
+    @DisplayName("approveReport()는")
+    @Nested
+    class Context_approveReport{
+
+        @DisplayName("Post를 삭제할 수 있다.")
+        @Test
+        void approved_report_willDeleted(){
+            // given
+            LocalDateTime deletedTime = LocalDateTime.now();
+            Post testPost = Post.builder()
+                .reportStatus(ReportStatus.REPORTED)
+                .build();
+
+            // when
+            testPost.approveReport(deletedTime);
+
+            // then
+            assertThat(testPost.getDeletedAt()).isEqualTo(deletedTime);
+            assertThat(testPost.isDeleted()).isTrue();
+        }
+
+        @DisplayName("ReportStatus가 Reported 가 아니면 approve 할 수 없다.")
+        @EnumSource(value = ReportStatus.class, names = {"NORMAL", "REPORT_REJECTED"})
+        @ParameterizedTest
+        void post_isNotReported_willNotChanged(ReportStatus reportStatus){
+            // given
+            LocalDateTime deletedTime = LocalDateTime.now();
+            Post testPost = Post.builder()
+                .reportStatus(reportStatus)
+                .build();
+
+            // when
+            testPost.approveReport(deletedTime);
+
+            // then
+            assertThat(testPost.getDeletedAt()).isNull();
+            assertThat(testPost.isDeleted()).isFalse();
+        }
+    }
+
+    @DisplayName("rejectReport()는")
+    @Nested
+    class Context_rejectReport{
+
+        @DisplayName("ReportStatus를 REPORT_REJECTED로 변경할 수 있다.")
+        @Test
+        void reportStatus_isReported_willChangedToReportRejected(){
+            // given
+            Post testPost = Post.builder()
+                .reportStatus(ReportStatus.REPORTED)
+                .build();
+
+            // when
+            testPost.rejectReport();
+
+            // then
+            assertThat(testPost.getReportStatus()).isSameAs(ReportStatus.REPORT_REJECTED);
+        }
+
+        @DisplayName("일반적인 게시글은 Reject할 수 없다 변경할 수 없다.")
+        @EnumSource(value = ReportStatus.class, names = {"NORMAL", "REPORT_REJECTED"})
+        @ParameterizedTest
+        void post_isNotReported_willNotChanged(ReportStatus reportStatus){
+            // given
+            Post testPost = Post.builder()
+                .reportStatus(reportStatus)
+                .build();
+
+            // when
+            testPost.rejectReport();
+
+            // then
+            assertThat(testPost.getReportStatus()).isSameAs(reportStatus);
+        }
+    }
+
+    @DisplayName("delete()는")
+    @Nested
+    class Context_delete{
+
+        @DisplayName("게시글을 삭제할 수 있다.")
+        @Test
+        void post_deletedAt_willSetNow(){
+            // given
+            LocalDateTime deletedAt = LocalDateTime.now();
+            Post testPost = Post.builder()
+                .build();
+
+            // when
+            testPost.delete(deletedAt);
+
+            // then
+            assertThat(testPost.getDeletedAt()).isEqualTo(deletedAt);
+            assertThat(testPost.isDeleted()).isTrue();
+        }
+
+        @DisplayName("이미 삭제된 게시글은 삭제 시간을 갱신할 수 없다.")
+        @Test
+        void alreadyDeletedPost_deletedAt_willNotSettingNow(){
+            // given
+            LocalDateTime deletedAt = LocalDateTime.now();
+            Post testPost = Post.builder()
+                .build();
+            testPost.delete(deletedAt);
+
+            // when
+            LocalDateTime newDeletedAt = LocalDateTime.now();
+            testPost.delete(newDeletedAt);
+
+            // then
+            assertThat(testPost.getDeletedAt()).isEqualTo(deletedAt);
+            assertThat(testPost.getDeletedAt()).isNotEqualTo(newDeletedAt);
+            assertThat(testPost.isDeleted()).isTrue();
+        }
+    }
+
+    @DisplayName("restore()는")
+    @Nested
+    class Context_restore{
+
+        @DisplayName("게시글을 복원할 수 있다.")
+        @Test
+        void post_deletedAt_willRemove(){
+            // given
+            LocalDateTime deletedAt = LocalDateTime.now();
+            Post testPost = Post.builder()
+                .build();
+
+            // when
+            testPost.delete(deletedAt);
+            testPost.restore();
+
+            // then
+            assertThat(testPost.getDeletedAt()).isNull();
+        }
     }
 }
