3cf0ea56d10bfb6b6ce6608fa1e884a027cb19f1
Nine-JH <rnwkgus2@gmail.com>
2023-08-23 18:55:04 +0900

    [feature] 게시글을 삭제할 수 있다.
    - 게시글 삭제 기능 추가.
    - 테스트 로직 추가

--- src/main/java/com/fasttime/domain/post/service/PostCommandService.java
+++ src/main/java/com/fasttime/domain/post/service/PostCommandService.java
@@ -1,5 +1,7 @@
 package com.fasttime.domain.post.service;
 
+import com.fasttime.domain.member.entity.Member;
+import com.fasttime.domain.member.repository.MemberRepository;
 import com.fasttime.domain.post.dto.service.request.PostCreateServiceDto;
 import com.fasttime.domain.post.dto.service.request.PostDeleteServiceDto;
 import com.fasttime.domain.post.dto.service.request.PostUpdateServiceDto;
@@ -13,15 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PostCommandService {
 
+    private final MemberRepository memberRepository;
     private final PostRepository postRepository;
 
-    public PostCommandService(PostRepository postRepository) {
+    public PostCommandService(MemberRepository memberRepository, PostRepository postRepository) {
+        this.memberRepository = memberRepository;
         this.postRepository = postRepository;
     }
 
     public PostResponseDto writePost(PostCreateServiceDto serviceDto) {
 
-        Post createdPost = Post.createNewPost(null, serviceDto.getTitle(), serviceDto.getContent(),
+        Member member = memberRepository.findById(serviceDto.getMemberId())
+            .orElseThrow(() -> new IllegalArgumentException("회원 정보가 없습니다."));
+
+        Post createdPost = Post.createNewPost(member, serviceDto.getTitle(), serviceDto.getContent(),
             false);
 
         Post savedPost = postRepository.save(createdPost);
