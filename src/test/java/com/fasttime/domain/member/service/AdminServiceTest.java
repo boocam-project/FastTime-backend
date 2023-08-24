fdb3ec18046f2598e652756f25fcec1aee115148
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-24 16:37:53 +0900

    [Refactor] 바뀐 메소드로 TEST 적용 및 AccessException Test 추가

--- src/test/java/com/fasttime/domain/member/service/AdminServiceTest.java
+++ src/test/java/com/fasttime/domain/member/service/AdminServiceTest.java
@@ -6,6 +6,8 @@ import com.fasttime.domain.post.entity.Post;
 import com.fasttime.domain.post.entity.ReportStatus;
 import com.fasttime.domain.post.repository.PostRepository;
 import com.fasttime.domain.post.service.PostCommandService;
+import java.rmi.AccessException;
+import java.time.LocalDateTime;
 import java.util.List;
 import javax.transaction.Transactional;
 import org.assertj.core.api.Assertions;
@@ -41,13 +43,13 @@ public class AdminServiceTest {
             PostResponseDto postResponseDto2 = postCommandService.writePost(dto2);
             Post post1 = postRepository.findById(postResponseDto1.getId()).get();
             Post post2 = postRepository.findById(postResponseDto2.getId()).get();
-            post1.ChangeReportStatus(ReportStatus.REPORTED);
-            post2.ChangeReportStatus(ReportStatus.REPORTED);
+            post1.report();
+            post2.report();
+            post1.approveReport(LocalDateTime.now());
+            post2.approveReport(LocalDateTime.now());
             // when
             List<Post> postList = adminService.FindReportedPost();
-            for (Post post : postList) {
-                System.out.println(post.getTitle());
-            }
+
             //then
             Assertions.assertThat(postList.get(0)).isEqualTo(post1);
             Assertions.assertThat(postList.get(1)).isEqualTo(post2);
@@ -75,11 +77,14 @@ public class AdminServiceTest {
     class PostDetail{
         @DisplayName("삭제할 수 있다.")
         @Test
-        void Delete_willSuccess(){
+        void Delete_willSuccess() throws AccessException {
             //given
             PostCreateServiceDto dto1 = new PostCreateServiceDto
                 (0L, "testTitle1", "testContent1", false);
             PostResponseDto dto = postCommandService.writePost(dto1);
+            Post post1 = postRepository.findById(dto.getId()).get();
+            post1.report();
+            post1.approveReport(LocalDateTime.now());
             //when
             adminService.DeletePost(dto.getId());
             //then
@@ -87,18 +92,44 @@ public class AdminServiceTest {
         }
         @DisplayName("검토완료로 바꿀 수 있다.")
         @Test
-        void Pass_willSuccess(){
+        void Pass_willSuccess() throws AccessException {
             //given
             PostCreateServiceDto dto1 = new PostCreateServiceDto
                 (0L, "testTitle1", "testContent1", false);
             PostResponseDto postResponseDto1 = postCommandService.writePost(dto1);
             Post post1 = postRepository.findById(postResponseDto1.getId()).get();
-            post1.ChangeReportStatus(ReportStatus.REPORTED);
+            post1.report();
+            post1.approveReport(LocalDateTime.now());
             //when
             adminService.PassPost(post1.getId());
+
             //then
             Assertions.assertThat(postRepository.findById(post1.getId()).get().getReportStatus())
-                .isEqualTo(ReportStatus.REPORTE_ABORTED);
+                .isEqualTo(ReportStatus.REPORT_REJECTED);
+        }
+        //IllegalArgumentException
+        @DisplayName("잘못된 접근으로 바꿀 수 없다.")
+        @Test
+        void _willFail() throws AccessException {
+            //given
+            PostCreateServiceDto dto1 = new PostCreateServiceDto
+                (0L, "testTitle1", "testContent1", false);
+            PostResponseDto postResponseDto1 = postCommandService.writePost(dto1);
+            Post post1 = postRepository.findById(postResponseDto1.getId()).get();
+            //when ,then
+            Assertions.assertThatThrownBy(() -> adminService.PassPost(post1.getId()));
+        }
+        //AccessException
+        @DisplayName("신고가 되지 않는 게시물에 접근하여 바꿀 수 없다.")
+        @Test
+        void Access_willFail() throws AccessException {
+            //given
+            PostCreateServiceDto dto1 = new PostCreateServiceDto
+                (0L, "testTitle1", "testContent1", false);
+            PostResponseDto postResponseDto1 = postCommandService.writePost(dto1);
+            Post post1 = postRepository.findById(postResponseDto1.getId()).get();
+            //when ,then
+            Assertions.assertThatThrownBy(() -> adminService.PassPost(post1.getId()));
         }
     }
 
