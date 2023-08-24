51d1e974e617911cac793c3ce832bc8c42f4086e
JeongUijeong <jeong275117@gmail.com>
2023-08-24 13:34:01 +0900

    [feat] 빌더를 사용한 신고 엔터티 생성 기능 구현

--- src/main/java/com/fasttime/domain/report/entity/Report.java
+++ src/main/java/com/fasttime/domain/report/entity/Report.java
@@ -2,6 +2,7 @@ package com.fasttime.domain.report.entity;
 
 import com.fasttime.domain.member.entity.Member;
 import com.fasttime.domain.post.entity.Post;
+import com.fasttime.domain.report.dto.ReportDTO;
 import com.fasttime.global.common.BaseTimeEntity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,4 +31,16 @@ public class Report extends BaseTimeEntity {
     @JoinColumn(name = "post_id")
     @ManyToOne
     private Post post;
+
+    @Builder
+    public Report(Long id, Member member, Post post) {
+        this.id = id;
+        this.member = member;
+        this.post = post;
+    }
+
+    public ReportDTO toDTO() {
+        return ReportDTO.builder().id(this.id).memberId(this.member.getId())
+            .postId(this.post.getId()).build();
+    }
 }
