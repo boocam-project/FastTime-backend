93d6ff6633c0485bbbf10116012c80e15a5ac2e3
JeongUijeong <jeong275117@gmail.com>
2023-08-24 15:51:25 +0900

    [refactor] 컨트롤러 어드바이스 수정
    - 제한자 수정
    - bindException, illegalArgException 예외 처리 추가

--- src/main/java/com/fasttime/domain/report/contoller/ReportRestControllerAdvice.java
+++ src/main/java/com/fasttime/domain/report/contoller/ReportRestControllerAdvice.java
@@ -6,23 +6,39 @@ import com.fasttime.global.util.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-public class ReportRestControllerAdvice {
+class ReportRestControllerAdvice {
 
     @ExceptionHandler
-    public ResponseEntity<ResponseDTO> duplicateReportException(DuplicateReportException e) {
+    ResponseEntity<ResponseDTO> duplicateReportException(DuplicateReportException e) {
         log.error("DuplicateReportException: " + e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ResponseDTO.res(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ResponseDTO> alreadyDeletedPostException(AlreadyDeletedPostException e) {
+    ResponseEntity<ResponseDTO> alreadyDeletedPostException(AlreadyDeletedPostException e) {
         log.error("PostAlreadyDeletedException: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ResponseDTO.res(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ResponseDTO> bindException(BindException e) {
+        log.error("BindException: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ResponseDTO.res(HttpStatus.BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ResponseDTO> illegalArgException(IllegalArgumentException e) {
+        log.error("IllegalArgumentException: " + e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ResponseDTO.res(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
