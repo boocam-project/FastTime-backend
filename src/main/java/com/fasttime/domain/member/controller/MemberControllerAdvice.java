5aa08cde51b09c9819e6d8c3cb7a3d2cc4103710
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-24 15:47:38 +0900

    [Refactor] 멤버 Controller Advice ResponseDTO로 변경

--- src/main/java/com/fasttime/domain/member/controller/MemberControllerAdvice.java
+++ src/main/java/com/fasttime/domain/member/controller/MemberControllerAdvice.java
@@ -17,26 +17,20 @@ public class MemberControllerAdvice {
 
     @ExceptionHandler   //검증 실패 예외
     public ResponseEntity<ResponseDTO> bindException(BindException e) {
-        Map<String, Object> message = new HashMap<>();
-        message.put("error", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
             ResponseDTO.res(HttpStatus.BAD_REQUEST,
                 e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
     }
     @ExceptionHandler   // 비밀번호가 틀리거나 비밀번호 재확인이 일치하지않는 예외
-    public ResponseEntity<?> BadCredentialsException(BadCredentialsException e) {
-        Map<String, Object> message = new HashMap<>();
-        message.put("error", e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(message);
+    public ResponseEntity<ResponseDTO> BadCredentialsException(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ResponseDTO.res(HttpStatus.BAD_REQUEST,
+                e.getMessage()));
     }
     @ExceptionHandler //등록되지 않는 이메일로 접근시 발생하는 예외
-    public ResponseEntity<?> UserNotFoundException(UserNotFoundException e) {
-        Map<String, Object> message = new HashMap<>();
-        message.put("error", e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(message);
+    public ResponseEntity<ResponseDTO> UserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ResponseDTO.res(HttpStatus.BAD_REQUEST,
+                e.getMessage()));
     }
 }
