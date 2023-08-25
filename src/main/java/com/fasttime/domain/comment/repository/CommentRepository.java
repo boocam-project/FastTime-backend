3be3edf65eeefad9531d0b18662acd483ec2903d
JeongUijeong <jeong275117@gmail.com>
2023-08-25 18:09:54 +0900

    [feat] 댓글 조회 기능 추가
    - 댓글을 Post로 조회하는 기능 추가
    - 댓글을 Member로 조회하는 기능 추가
    - 회원 조회에 MemberService를 사용하도록 수정
    - 추가한 기능 및 회원 조회 방법 수정에 따른 테스트 추가, 수정

--- src/main/java/com/fasttime/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/fasttime/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,15 @@
 package com.fasttime.domain.comment.repository;
 
 import com.fasttime.domain.comment.entity.Comment;
+import com.fasttime.domain.member.entity.Member;
+import com.fasttime.domain.post.entity.Post;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    Optional<List<Comment>> findAllByPost(Post post);
+
+    Optional<List<Comment>> findAllByMember(Member member);
 }
