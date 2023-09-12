d2d13303778411af7f47a36d5a3737c9f26fe560
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-09-12 17:32:46 +0900

    [Refactor] Exception 변경 Advice에 반영

--- src/main/java/com/fasttime/domain/member/controller/EmailControllerAdvice.java
+++ src/main/java/com/fasttime/domain/member/controller/EmailControllerAdvice.java
@@ -7,14 +7,16 @@ import javax.naming.AuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class EmailControllerAdvice {
-    @ExceptionHandler
-    public ResponseEntity<ResponseDTO> AuthenticationException(AuthenticationException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ResponseDTO.res(HttpStatus.BAD_REQUEST, e.getMessage()));
+    @ExceptionHandler   // 코드가 일치하지않는 예외
+    public ResponseEntity<ResponseDTO> BadCredentialsException(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ResponseDTO.res(HttpStatus.BAD_REQUEST,
+                e.getMessage()));
     }
 }
