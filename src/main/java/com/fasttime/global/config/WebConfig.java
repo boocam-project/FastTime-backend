1d5c8f014bede452d2ca6e4fd4abfac5c697ee79
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-09-20 12:59:07 +0900

    [Fix] 로그인 인터셉터를 적용할 URL 변경

--- src/main/java/com/fasttime/global/config/WebConfig.java
+++ src/main/java/com/fasttime/global/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.fasttime.global.config;
 import com.fasttime.global.interceptor.AdminCheckInterceptor;
 import com.fasttime.global.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile({"local, prod"})
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -16,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
             .addPathPatterns("/v1/admin");
         registry.addInterceptor(new LoginCheckInterceptor())
             .order(2)
-            .addPathPatterns("/api/v1/post", "/api/v1/comment",
-                "v1/retouch-member", "v1/logout", "v1/delete");
+            .addPathPatterns("/api/v1/comment/**",
+                "v1/retouch-member", "api/v1/logout", "v1/delete");
     }
 }
