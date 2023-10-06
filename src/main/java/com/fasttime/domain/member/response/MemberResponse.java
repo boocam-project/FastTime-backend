5a6fe170b0414503412447611c4e9cb967c5f98f
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-10-06 18:14:01 +0900

    [fix] MemberResponse ID 추가 및 email 삭제

--- src/main/java/com/fasttime/domain/member/response/MemberResponse.java
+++ src/main/java/com/fasttime/domain/member/response/MemberResponse.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberResponse {
+
     private Long id;
-    private String email;
     private String nickname;
 }
