562ea729ccee73ac2daea324e6caacc08a1a3e67
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-10-09 11:37:06 +0900

    [fix] Method OPTIONS 수정

--- src/main/java/com/fasttime/global/config/WebConfig.java
+++ src/main/java/com/fasttime/global/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE","OPTION");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS");
     }
 
 }
