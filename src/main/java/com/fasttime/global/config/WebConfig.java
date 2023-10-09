2bb7f834425562ae7d1836fe8b1e8c33f4338e53
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-10-09 12:22:44 +0900

    [fix] allowedOriginPatterns 메소드 변경

--- src/main/java/com/fasttime/global/config/WebConfig.java
+++ src/main/java/com/fasttime/global/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS");
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS");
     }
 
 }
