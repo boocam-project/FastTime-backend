02443e9a715fd5328bd920b61dd8160343649242
JeongUijeong <jeong275117@gmail.com>
2023-10-11 19:59:20 +0900

    [refactor] 댓글 등록, 조회, 수정 시 응답 DTO 세분화
    - 게시글에서 댓글 등록, 조회, 수정 시 PostCommentResponseDTO 응답하도록 수정
    - 마이페이지에서 댓글 조회 시 MyPageCommentResponseDTO 응답하도록 수정
    - 게시글 삭제 시 댓글 데이터 응답하지 않도록 수정

--- src/main/java/com/fasttime/domain/comment/service/CommentService.java
+++ src/main/java/com/fasttime/domain/comment/service/CommentService.java
@@ -3,7 +3,8 @@ package com.fasttime.domain.comment.service;
 import com.fasttime.domain.comment.dto.request.CreateCommentRequest;
 import com.fasttime.domain.comment.dto.request.DeleteCommentRequest;
 import com.fasttime.domain.comment.dto.request.UpdateCommentRequest;
-import com.fasttime.domain.comment.dto.response.CommentResponseDTO;
+import com.fasttime.domain.comment.dto.response.MyPageCommentResponseDTO;
+import com.fasttime.domain.comment.dto.response.PostCommentResponseDTO;
 import com.fasttime.domain.comment.entity.Comment;
 import com.fasttime.domain.comment.exception.CommentNotFoundException;
 import com.fasttime.domain.comment.repository.CommentRepository;
@@ -32,7 +33,7 @@ public class CommentService {
      * @param req 등록할 댓글 정보가 담긴 객체
      * @return 등록한 댓글 DTO
      */
-    public CommentResponseDTO createComment(CreateCommentRequest req) {
+    public PostCommentResponseDTO createComment(CreateCommentRequest req) {
         Comment parentComment = null;
         // 대댓글인 경우
         if (req.getParentCommentId() != null) {
@@ -42,7 +43,7 @@ public class CommentService {
                 Comment.builder().post(postQueryService.findById(req.getPostId()))
                     .member(memberService.getMember(req.getMemberId())).content(req.getContent())
                     .anonymity(req.getAnonymity()).parentComment(parentComment).build())
-            .toResponseDTO();
+            .toPostCommentResponseDTO();
     }
 
     /**
@@ -51,7 +52,7 @@ public class CommentService {
      * @param memberId 댓글 리스트 조회의 기준이 될 회원 ID
      * @return 회원이 등록한 댓글 DTO 리스트
      */
-    public List<CommentResponseDTO> getCommentsByMemberId(long memberId) {
+    public List<MyPageCommentResponseDTO> getCommentsByMemberId(long memberId) {
         return getCommentsByMember(memberService.getMember(memberId));
     }
 
@@ -61,7 +62,7 @@ public class CommentService {
      * @param postId 댓글 리스트 조회의 기준이 될 게시글 ID
      * @return 게시글에 등록된 댓글 DTO 리스트
      */
-    public List<CommentResponseDTO> getCommentsByPostId(long postId) {
+    public List<PostCommentResponseDTO> getCommentsByPostId(long postId) {
         return getCommentsByPost(postQueryService.findById(postId));
     }
 
@@ -71,11 +72,11 @@ public class CommentService {
      * @param req 수정할 댓글 정보와 수정 내용이 담긴 객체
      * @return 수정한 댓글 DTO
      */
-    public CommentResponseDTO updateComment(UpdateCommentRequest req) {
+    public PostCommentResponseDTO updateComment(UpdateCommentRequest req) {
         Comment comment = commentRepository.findById(req.getId())
             .orElseThrow(CommentNotFoundException::new);
         comment.updateContent(req.getContent());
-        return comment.toResponseDTO();
+        return comment.toPostCommentResponseDTO();
     }
 
     /**
@@ -84,11 +85,10 @@ public class CommentService {
      * @param req 삭제할 댓글 ID가 담긴 객체
      * @return 삭제한 댓글 DTO
      */
-    public CommentResponseDTO deleteComment(DeleteCommentRequest req) {
+    public void deleteComment(DeleteCommentRequest req) {
         Comment comment = commentRepository.findById(req.getId())
             .orElseThrow(CommentNotFoundException::new);
         comment.deleteComment();
-        return comment.toResponseDTO();
     }
 
     /**
@@ -107,11 +107,11 @@ public class CommentService {
      * @param post 댓글 리스트를 조회할 게시글 Entity
      * @return 게시글에 등록된 댓글 리스트
      */
-    public List<CommentResponseDTO> getCommentsByPost(Post post) {
-        List<CommentResponseDTO> comments = new ArrayList<>();
+    public List<PostCommentResponseDTO> getCommentsByPost(Post post) {
+        List<PostCommentResponseDTO> comments = new ArrayList<>();
         List<Comment> list = commentRepository.findAllByPost(post).orElseGet(ArrayList::new);
         for (Comment comment : list) {
-            comments.add(comment.toResponseDTO());
+            comments.add(comment.toPostCommentResponseDTO());
         }
         return comments;
     }
@@ -122,11 +122,11 @@ public class CommentService {
      * @param member 댓글 리스트 조회의 기준이 될 회원 Entity
      * @return 회원이 등록한 댓글 리스트
      */
-    public List<CommentResponseDTO> getCommentsByMember(Member member) {
-        List<CommentResponseDTO> comments = new ArrayList<>();
+    public List<MyPageCommentResponseDTO> getCommentsByMember(Member member) {
+        List<MyPageCommentResponseDTO> comments = new ArrayList<>();
         List<Comment> list = commentRepository.findAllByMember(member).orElseGet(ArrayList::new);
         for (Comment comment : list) {
-            comments.add(comment.toResponseDTO());
+            comments.add(comment.toMyPageCommentResponseDTO());
         }
         return comments;
     }
