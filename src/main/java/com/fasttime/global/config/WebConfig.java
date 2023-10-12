97681f6bfc46c5b12848d4b0e94c464a671c3446
Nine-JH <rnwkgus2@gmail.com>
2023-10-12 14:01:48 +0900

    [fix] Test profile 제외
    Motivation:
    - 테스트 환경에서 로그인 인터셉터 적용을 제외하기 위해 설정.
    
    Modification:
    
    -`@Profile("!test")` 추가

--- src/main/java/com/fasttime/global/config/WebConfig.java
+++ src/main/java/com/fasttime/global/config/WebConfig.java
@@ -3,14 +3,16 @@ package com.fasttime.global.config;
 import com.fasttime.global.interceptor.AdminCheckInterceptor;
 import com.fasttime.global.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("!test")
 public class WebConfig implements WebMvcConfigurer {
 
-        @Override
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AdminCheckInterceptor())
             .order(1)
@@ -20,6 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
             .addPathPatterns("/api/v1/comment/**",
                 "/api/v1/retouch-member", "/api/v1/logout", "/api/v1/delete");
     }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
