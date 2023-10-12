a5ef831d097ec555ce23165cdfeb55d5e5a1b261
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-10-12 20:46:32 +0900

    [refactor] responseDTO로 변경

--- src/main/java/com/fasttime/domain/member/service/AdminService.java
+++ src/main/java/com/fasttime/domain/member/service/AdminService.java
@@ -1,10 +1,13 @@
 package com.fasttime.domain.member.service;
 
+import com.fasttime.domain.post.dto.service.response.PostDetailResponseDto;
+import com.fasttime.domain.post.dto.service.response.PostsResponseDto;
 import com.fasttime.domain.post.entity.Post;
 import com.fasttime.domain.post.entity.ReportStatus;
 import com.fasttime.domain.post.repository.PostRepository;
 import java.rmi.AccessException;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,33 +19,38 @@ public class AdminService {
 
     private final PostRepository postRepository;
 
-    public List<Post> findReportedPost(){
-        return postRepository.findAllByReportStatus(ReportStatus.REPORTED);
+    public List<PostsResponseDto> findReportedPost(){
+        return postRepository.findAllByReportStatus(ReportStatus.REPORTED).stream()
+            .map(post -> PostsResponseDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .nickname(post.getMember().getNickname())
+                .anonymity(post.isAnonymity())
+                .likeCount(post.getLikeCount())
+                .hateCount(post.getHateCount())
+                .createdAt(post.getCreatedAt())
+                .lastModifiedAt(post.getUpdatedAt())
+                .build())
+            .collect(Collectors.toList());
     }
 
-    public Post findOneReportedPost(Long id) throws AccessException {
+    public PostDetailResponseDto findOneReportedPost(Long id) throws AccessException {
         Post post = postRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("게시글이 없습니다."));
         System.out.println(post.getReportStatus());
         if (!post.getReportStatus().equals(ReportStatus.REPORTED)){
             throw new AccessException("잘못된 접근입니다.");
         }
-        return post;
+        return PostDetailResponseDto.entityToDto(post);
     }
-    public void deletePost(Long id) throws AccessException {
+    public void deletePost(Long id){
         Post post = postRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("게시글이 없습니다."));
-        if (!post.getReportStatus().equals(ReportStatus.REPORTED)){
-            throw new AccessException("잘못된 접근입니다.");
-        }
         postRepository.delete(post);
     }
-    public void passPost(Long id) throws AccessException {
+    public void passPost(Long id) {
         Post post = postRepository.findById(id).
             orElseThrow(()-> new IllegalArgumentException("게시글이 없습니다."));
-        if (!post.getReportStatus().equals(ReportStatus.REPORTED)){
-            throw new AccessException("잘못된 접근입니다.");
-        }
         post.rejectReport();
     }
 
