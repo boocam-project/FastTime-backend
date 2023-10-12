9f7a4270a6f08d2db300fce2945c16257a3c0d07
Nine-JH <rnwkgus2@gmail.com>
2023-10-12 14:16:54 +0900

    [fix] Test profile 제외
    Motivation:
    - PATCH Method를 사용함에 따라 cors 허용에 추가.
    
    Modification:
    
    - allowedMethods("PATCH") 추가

--- src/main/java/com/fasttime/global/config/WebConfig.java
+++ src/main/java/com/fasttime/global/config/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 
 }
