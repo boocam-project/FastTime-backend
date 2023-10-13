497974b10dd18a23232e4f30307ab694803d57d5
JeongUijeong <jeong275117@gmail.com>
2023-10-13 22:31:41 +0900

    [fix] CORS 관련 설정 수정

--- src/main/java/com/fasttime/global/config/WebConfig.java
+++ src/main/java/com/fasttime/global/config/WebConfig.java
@@ -27,8 +27,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+            .allowedOriginPatterns("http://localhost:3000", "https://localhost:3000",
+                "http://127.0.0.1:3000", "https://127.0.0.1:3000", "http://localhost:5173",
+                "https://localhost:5173", "http://127.0.0.1:5173", "https://127.0.0.1:5173")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowCredentials(true)
+            .exposedHeaders("*");
     }
 
 }
