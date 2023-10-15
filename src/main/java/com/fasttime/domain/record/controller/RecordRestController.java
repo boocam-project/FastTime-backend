086451dd1d813164768fd6c089cf9daca9624d51
JeongUijeong <jeong275117@gmail.com>
2023-10-16 04:47:52 +0900

    [feat] 좋아요/싫어요 API 컨트롤러 구현 및 컨트롤러 어드바이스 구현

---
+++ src/main/java/com/fasttime/domain/record/controller/RecordRestController.java
@@ -0,0 +1,52 @@
+package com.fasttime.domain.record.controller;
+
+import com.fasttime.domain.record.dto.request.CreateRecordRequestDTO;
+import com.fasttime.domain.record.dto.request.DeleteRecordRequestDTO;
+import com.fasttime.domain.record.service.RecordService;
+import com.fasttime.global.util.ResponseDTO;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/record")
+public class RecordRestController {
+
+    private final RecordService recordService;
+
+    @PostMapping("/like")
+    public ResponseEntity<ResponseDTO<Void>> createLike(
+        @Valid @RequestBody CreateRecordRequestDTO createRecordRequestDTO) {
+        log.info("CreateRecordRequest: " + createRecordRequestDTO + "(like)");
+        recordService.createRecord(createRecordRequestDTO, true);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ResponseDTO.res(HttpStatus.CREATED, "좋아요를 성공적으로 접수했습니다.", null));
+    }
+
+    @PostMapping("/hate")
+    public ResponseEntity<ResponseDTO<Void>> createHate(
+        @Valid @RequestBody CreateRecordRequestDTO createRecordRequestDTO) {
+        log.info("CreateRecordRequest: " + createRecordRequestDTO + "(hate)");
+        recordService.createRecord(createRecordRequestDTO, false);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ResponseDTO.res(HttpStatus.CREATED, "싫어요를 성공적으로 접수했습니다.", null));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ResponseDTO<Void>> deleteRecord(
+        @Valid @RequestBody DeleteRecordRequestDTO deleteRecordRequestDTO) {
+        log.info("DeleteRecordRequest: " + deleteRecordRequestDTO);
+        recordService.deleteRecord(deleteRecordRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ResponseDTO.res(HttpStatus.OK, "좋아요/싫어요를 성공적으로 취소했습니다.", null));
+    }
+}
---
+++ src/main/java/com/fasttime/domain/record/controller/RecordRestControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.fasttime.domain.record.controller;
+
+import com.fasttime.domain.record.exception.DuplicateRecordException;
+import com.fasttime.global.util.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+class RecordRestControllerAdvice {
+
+    @ExceptionHandler
+    ResponseEntity<ResponseDTO<Object>> duplicateRecordException(DuplicateRecordException e) {
+        log.error("DuplicateRecordException: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ResponseDTO.res(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+}
