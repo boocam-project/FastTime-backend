8b7171a5561aaf3d3b7e0c19f7e104c12a5e7da6
JeongUijeong <jeong275117@gmail.com>
2023-10-16 19:15:03 +0900

    [feat] 좋아요/싫어요 조회 API 추가

--- src/main/java/com/fasttime/domain/record/controller/RecordRestController.java
+++ src/main/java/com/fasttime/domain/record/controller/RecordRestController.java
@@ -1,15 +1,19 @@
 package com.fasttime.domain.record.controller;
 
+import com.fasttime.domain.record.dto.RecordDTO;
 import com.fasttime.domain.record.dto.request.CreateRecordRequestDTO;
 import com.fasttime.domain.record.dto.request.DeleteRecordRequestDTO;
 import com.fasttime.domain.record.service.RecordService;
 import com.fasttime.global.util.ResponseDTO;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +43,16 @@ public class RecordRestController {
         recordService.createRecord(createRecordRequestDTO, false);
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(ResponseDTO.res(HttpStatus.CREATED, "싫어요를 성공적으로 접수했습니다.", null));
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<ResponseDTO<RecordDTO>> getRecord(@PathVariable long postId,
+        HttpSession session) {
+        long memberId = (long) session.getAttribute("MEMBER");
+        log.info("getRecord: " + postId + "(postId) + " + memberId + "(memberId)");
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDTO.res(HttpStatus.OK, "좋아요/싫어요를 성공적으로 조회했습니다.",
+                recordService.getRecord(memberId, postId)));
     }
 
     @DeleteMapping
