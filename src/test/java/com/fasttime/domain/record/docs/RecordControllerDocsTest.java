a3e96732624547a68da5c4c07db81f90938f8b07
JeongUijeong <jeong275117@gmail.com>
2023-10-16 19:16:07 +0900

    [test] 좋아요/싫어요 조회 API 문서화 테스트 코드 추가

--- src/test/java/com/fasttime/domain/record/docs/RecordControllerDocsTest.java
+++ src/test/java/com/fasttime/domain/record/docs/RecordControllerDocsTest.java
@@ -2,6 +2,7 @@ package com.fasttime.domain.record.docs;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -11,20 +12,24 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.snippet.Attributes.key;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasttime.docs.RestDocsSupport;
 import com.fasttime.domain.record.controller.RecordRestController;
+import com.fasttime.domain.record.dto.RecordDTO;
 import com.fasttime.domain.record.dto.request.CreateRecordRequestDTO;
 import com.fasttime.domain.record.dto.request.DeleteRecordRequestDTO;
 import com.fasttime.domain.record.service.RecordService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -57,11 +62,11 @@ public class RecordControllerDocsTest extends RestDocsSupport {
             .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andDo(
             document("record-like-create", preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()), requestFields(
-                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").attributes(
-                        key("constraints").value(
+                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자")
+                        .attributes(key("constraints").value(
                             createRecordRequestConstraints.descriptionsForProperty("memberId"))),
-                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자").attributes(
-                        key("constraints").value(
+                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
+                        .attributes(key("constraints").value(
                             createRecordRequestConstraints.descriptionsForProperty("postId")))),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
@@ -84,16 +89,46 @@ public class RecordControllerDocsTest extends RestDocsSupport {
             .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andDo(
             document("record-hate-create", preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()), requestFields(
-                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").attributes(
-                        key("constraints").value(
+                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자")
+                        .attributes(key("constraints").value(
                             createRecordRequestConstraints.descriptionsForProperty("memberId"))),
-                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자").attributes(
-                        key("constraints").value(
+                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
+                        .attributes(key("constraints").value(
                             createRecordRequestConstraints.descriptionsForProperty("postId")))),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
                     fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
                     fieldWithPath("data").type(JsonFieldType.NULL).description("응답데이터"))));
+    }
+
+    @DisplayName("좋아요/싫어요 조회 API 문서화")
+    @Test
+    void getRecord() throws Exception {
+        // given
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("MEMBER", 1L);
+        RecordDTO recordDTO = RecordDTO.builder().id(1L).memberId(1L).postId(1L).isLike(true)
+            .build();
+        given(recordService.getRecord(any(Long.class), any(Long.class))).willReturn(recordDTO);
+
+        // when, then
+        mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/v1/record/{postId}", 1L).session(session))
+            .andExpect(status().isOk()).andDo(
+                document("record-get", preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    pathParameters(parameterWithName("postId").description("게시글 식별자")), responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답데이터"),
+                        fieldWithPath("data.id").type(JsonFieldType.NUMBER).optional()
+                            .description("좋아요/싫어요 식별자"),
+                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).optional()
+                            .description("회원 식별자"),
+                        fieldWithPath("data.postId").type(JsonFieldType.NUMBER).optional()
+                            .description("게시글 식별자"),
+                        fieldWithPath("data.like").type(JsonFieldType.BOOLEAN)
+                            .description("좋아요(1), 싫어요(0)"))));
     }
 
     @DisplayName("좋아요/싫어요 취소 API 문서화")
@@ -110,11 +145,11 @@ public class RecordControllerDocsTest extends RestDocsSupport {
             .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(
             document("record-delete", preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()), requestFields(
-                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").attributes(
-                        key("constraints").value(
+                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자")
+                        .attributes(key("constraints").value(
                             deleteRecordRequestConstraints.descriptionsForProperty("memberId"))),
-                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자").attributes(
-                        key("constraints").value(
+                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
+                        .attributes(key("constraints").value(
                             deleteRecordRequestConstraints.descriptionsForProperty("postId")))),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
