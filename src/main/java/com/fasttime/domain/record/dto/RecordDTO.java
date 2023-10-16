e5fb4856bea0d1d77c86b554ab7f1fddc7531d37
JeongUijeong <jeong275117@gmail.com>
2023-10-16 19:09:31 +0900

    [fix] 자료형 수정

--- src/main/java/com/fasttime/domain/record/dto/RecordDTO.java
+++ src/main/java/com/fasttime/domain/record/dto/RecordDTO.java
@@ -1,7 +1,5 @@
 package com.fasttime.domain.record.dto;
 
-import com.fasttime.domain.member.entity.Member;
-import com.fasttime.domain.post.entity.Post;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,7 +8,7 @@ import lombok.Getter;
 public class RecordDTO {
 
     private Long id;
-    private Member member;
-    private Post post;
+    private Long memberId;
+    private Long postId;
     private boolean isLike;
 }
