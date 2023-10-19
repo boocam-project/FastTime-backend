d7568ed2f71933c83a10173a510f130ddf897bd9
JeongUijeong <jeong275117@gmail.com>
2023-10-19 11:05:33 +0900

    [refactor] 신고 요청 매핑 url 수정

--- src/main/java/com/fasttime/domain/report/contoller/ReportRestController.java
+++ src/main/java/com/fasttime/domain/report/contoller/ReportRestController.java
@@ -22,12 +22,12 @@ public class ReportRestController {
 
     private final ReportService reportService;
 
-    @PostMapping("/create")
-    public ResponseEntity<ResponseDTO<Void>> createReport(
+    @PostMapping
+    public ResponseEntity<ResponseDTO<Object>> createReport(
         @Valid @RequestBody CreateReportRequestDTO createReportRequestDTO, HttpSession session) {
         log.info("CreateReportRequest: " + createReportRequestDTO);
         reportService.createReport(createReportRequestDTO, (Long) session.getAttribute("MEMBER"));
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(ResponseDTO.res(HttpStatus.CREATED, "신고를 성공적으로 접수했습니다.", null));
+            .body(ResponseDTO.res(HttpStatus.CREATED, "신고를 성공적으로 접수했습니다."));
     }
 }
