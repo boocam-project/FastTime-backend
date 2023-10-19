2415d01153de82d4ff95ac4e0c5dd2d877d4be65
JeongUijeong <jeong275117@gmail.com>
2023-10-19 10:56:57 +0900

    [test] 좋아요/싫어요 API 문서화 테스트 코드 수정

--- src/test/java/com/fasttime/domain/record/docs/RecordControllerDocsTest.java
+++ src/test/java/com/fasttime/domain/record/docs/RecordControllerDocsTest.java
@@ -1,7 +1,6 @@
 package com.fasttime.domain.record.docs;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
@@ -47,69 +46,45 @@ public class RecordControllerDocsTest extends RestDocsSupport {
     ConstraintDescriptions deleteRecordRequestConstraints = new ConstraintDescriptions(
         DeleteRecordRequestDTO.class);
 
-    @DisplayName("좋아요 등록 API 문서화")
+    @DisplayName("좋아요/싫어요 등록 API 문서화")
     @Test
     void createLike() throws Exception {
         // given
-        CreateRecordRequestDTO request = CreateRecordRequestDTO.builder().memberId(1L).postId(1L)
+        CreateRecordRequestDTO request = CreateRecordRequestDTO.builder().postId(1L).isLike(true)
             .build();
         doNothing().when(recordService)
-            .createRecord(any(CreateRecordRequestDTO.class), anyBoolean());
+            .createRecord(any(CreateRecordRequestDTO.class), any(Long.class));
         String json = new ObjectMapper().writeValueAsString(request);
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("MEMBER", 1L);
 
         // when, then
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/record/like").content(json)
-            .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andDo(
-            document("record-like-create", preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()), requestFields(
-                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자")
-                        .attributes(key("constraints").value(
-                            createRecordRequestConstraints.descriptionsForProperty("memberId"))),
-                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
-                        .attributes(key("constraints").value(
-                            createRecordRequestConstraints.descriptionsForProperty("postId")))),
-                responseFields(
-                    fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
-                    fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
-                    fieldWithPath("data").type(JsonFieldType.NULL).description("응답데이터"))));
-    }
-
-    @DisplayName("싫어요 등록 API 문서화")
-    @Test
-    void createHate() throws Exception {
-        // given
-        CreateRecordRequestDTO request = CreateRecordRequestDTO.builder().memberId(1L).postId(1L)
-            .build();
-        doNothing().when(recordService)
-            .createRecord(any(CreateRecordRequestDTO.class), anyBoolean());
-        String json = new ObjectMapper().writeValueAsString(request);
-
-        // when, then
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/record/hate").content(json)
-            .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andDo(
-            document("record-hate-create", preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()), requestFields(
-                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자")
-                        .attributes(key("constraints").value(
-                            createRecordRequestConstraints.descriptionsForProperty("memberId"))),
-                    fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
-                        .attributes(key("constraints").value(
-                            createRecordRequestConstraints.descriptionsForProperty("postId")))),
-                responseFields(
-                    fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
-                    fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
-                    fieldWithPath("data").type(JsonFieldType.NULL).description("응답데이터"))));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/record").content(json)
+                .contentType(MediaType.APPLICATION_JSON).session(session))
+            .andExpect(status().isCreated()).andDo(
+                document("record-create", preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()), requestFields(
+                        fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
+                            .attributes(key("constraints").value(
+                                createRecordRequestConstraints.descriptionsForProperty("postId"))),
+                        fieldWithPath("isLike").type(JsonFieldType.BOOLEAN)
+                            .description("좋아요(true)/싫어요(false)").attributes(key("constraints").value(
+                                createRecordRequestConstraints.descriptionsForProperty("isLike")))),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NULL).description("응답데이터"))));
     }
 
     @DisplayName("좋아요/싫어요 조회 API 문서화")
     @Test
     void getRecord() throws Exception {
         // given
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute("MEMBER", 1L);
         RecordDTO recordDTO = RecordDTO.builder().id(1L).memberId(1L).postId(1L).isLike(true)
             .build();
         given(recordService.getRecord(any(Long.class), any(Long.class))).willReturn(recordDTO);
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("MEMBER", 1L);
 
         // when, then
         mockMvc.perform(
@@ -127,17 +102,17 @@ public class RecordControllerDocsTest extends RestDocsSupport {
                             .description("회원 식별자"),
                         fieldWithPath("data.postId").type(JsonFieldType.NUMBER).optional()
                             .description("게시글 식별자"),
-                        fieldWithPath("data.isLike").type(JsonFieldType.BOOLEAN)
-                            .description("좋아요(1), 싫어요(0)"))));
+                        fieldWithPath("data.isLike").type(JsonFieldType.BOOLEAN).optional()
+                            .description("좋아요(true)/싫어요(false)"))));
     }
 
     @DisplayName("좋아요/싫어요 취소 API 문서화")
     @Test
     void deleteRecord() throws Exception {
         // given
-        DeleteRecordRequestDTO request = DeleteRecordRequestDTO.builder().memberId(1L).postId(1L)
-            .build();
-        doNothing().when(recordService).deleteRecord(any(DeleteRecordRequestDTO.class));
+        DeleteRecordRequestDTO request = DeleteRecordRequestDTO.builder().postId(1L).build();
+        doNothing().when(recordService)
+            .deleteRecord(any(DeleteRecordRequestDTO.class), any(Long.class));
         String json = new ObjectMapper().writeValueAsString(request);
 
         // when, then
@@ -145,9 +120,6 @@ public class RecordControllerDocsTest extends RestDocsSupport {
             .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(
             document("record-delete", preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()), requestFields(
-                    fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자")
-                        .attributes(key("constraints").value(
-                            deleteRecordRequestConstraints.descriptionsForProperty("memberId"))),
                     fieldWithPath("postId").type(JsonFieldType.NUMBER).description("게시글 식별자")
                         .attributes(key("constraints").value(
                             deleteRecordRequestConstraints.descriptionsForProperty("postId")))),
