f183d969f025694d5da1f859a092bb359549738a
Nine-JH <rnwkgus2@gmail.com>
2023-10-24 13:40:31 +0900

    [feature] http request 로그 추가
    Motivation:
    - HTTP Log를 저장하게끔 filter config를 추가했습니다.
    
    Modification:
    - LoggingConfig.CommonsRequestLoggingFilter 추가

---
+++ src/main/java/com/fasttime/global/logging/LoggingConfig.java
@@ -0,0 +1,20 @@
+package com.fasttime.global.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST BODY: ");
+        return filter;
+    }
+}
