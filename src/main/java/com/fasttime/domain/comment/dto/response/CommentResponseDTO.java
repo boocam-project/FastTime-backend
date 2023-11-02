d030c5133f572c3160fc4f0f0bccd1f1e2a46df5
JeongUijeong <jeong275117@gmail.com>
2023-11-02 15:41:50 +0900

    [refactor] 댓글 응답 DTO를 하나로 통일

--- src/main/java/com/fasttime/domain/comment/dto/response/CommentResponseDTO.java
+++ src/main/java/com/fasttime/domain/comment/dto/response/CommentResponseDTO.java
@@ -1,13 +1,15 @@
 package com.fasttime.domain.comment.dto.response;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
-public class MyPageCommentResponseDTO {
+public class ArticleCommentResponseDTO {
 
     private Long id;
+    private Long memberId;
     private Long postId;
     private String nickname;
     private String content;
@@ -16,4 +18,5 @@ public class MyPageCommentResponseDTO {
     private String createdAt;
     private String updatedAt;
     private String deletedAt;
+    private List<ArticleCommentResponseDTO> childComments;
 }
