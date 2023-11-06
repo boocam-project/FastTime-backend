6a3d086391a802a632ff7e3ef5484a1e7b588e85
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-07 00:09:30 +0900

    [feat] Jwt 프로퍼티 구현

---
+++ src/main/java/com/fasttime/global/jwt/JwtProperties.java
@@ -0,0 +1,7 @@
+package com.fasttime.global.jwt;
+
+public class JwtProperties {
+    public static final int EXPIRATION_TIME =  1000 * 60 * 60; // 10분
+    public static final String COOKIE_NAME = "JWT-AUTHENTICATION";
+
+}
