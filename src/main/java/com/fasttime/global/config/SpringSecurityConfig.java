7704d4fb6b1b82486d2dd10dfa6bd9a5fc94aeb2
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-07 18:30:04 +0900

    [feat] config 토큰 처리 안되게 처리

--- src/main/java/com/fasttime/global/config/SpringSecurityConfig.java
+++ src/main/java/com/fasttime/global/config/SpringSecurityConfig.java
@@ -1,21 +1,12 @@
 package com.fasttime.global.config;
 
-import com.fasttime.domain.member.repository.MemberRepository;
-import com.fasttime.global.exception.AccessDeniedHandlerImpl;
-import com.fasttime.global.jwt.JwtAuthenticationFilter;
-import com.fasttime.global.jwt.JwtAuthorizationFilter;
-import com.fasttime.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 /**
@@ -25,31 +16,36 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final JwtProvider jwtProvider;
-    private final AccessDeniedHandlerImpl accessDeniedHandler;
-
+//    private final JwtProvider jwtProvider;
+//    private final AccessDeniedHandlerImpl accessDeniedHandler;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable() // basic authentication filter 비활성화 for 보안
-            .csrf().disable()
-            .formLogin().disable()
-            .rememberMe().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().authorizeRequests()
-            .antMatchers("/api/v1/join", "/api/v1/login", "/api/v1/admin/join", "/error")
-            .permitAll()
-            .antMatchers(HttpMethod.GET, "/api/v1/article").permitAll()
-            .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
-            .anyRequest().authenticated()
-            .and()
-            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
-                UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling()
-            .accessDeniedHandler(accessDeniedHandler);
-
-        http.logout().disable();
-//            .logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/logout"));
+        // basic authentication
+        http.httpBasic().disable(); // basic authentication filter 비활성화 for 보안
+        // cors 허용, csrf 보안을 위한 필터
+        http.cors().and().csrf().disable();
     }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic().disable() // basic authentication filter 비활성화 for 보안
+//            .csrf().disable()
+//            .formLogin().disable()
+//            .rememberMe().disable()
+//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//            .and().authorizeRequests()
+//            .antMatchers("/api/v1/join", "/api/v1/login", "/api/v1/admin/join", "/error")
+//            .permitAll()
+//            .antMatchers(HttpMethod.GET, "/api/v1/article").permitAll()
+//            .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
+//            .anyRequest().authenticated()
+//            .and()
+//            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
+//                UsernamePasswordAuthenticationFilter.class)
+//            .exceptionHandling()
+//            .accessDeniedHandler(accessDeniedHandler);
+//
+//        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/logout"));
+//    }
 
     @Override
     public void configure(WebSecurity web) {
