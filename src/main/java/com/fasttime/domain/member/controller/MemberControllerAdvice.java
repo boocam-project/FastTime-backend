e4fb4a4bc5b8f01e01733d35cd3175990bd2775f
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-09 19:09:08 +0900

    [feat] MemberControllerAdvice Exception 구체화로 인한 예외처리 핸들러 추가

--- src/main/java/com/fasttime/domain/member/controller/MemberControllerAdvice.java
+++ src/main/java/com/fasttime/domain/member/controller/MemberControllerAdvice.java
@@ -1,20 +1,25 @@
 package com.fasttime.domain.member.controller;
 
+import com.fasttime.domain.comment.exception.NotCommentAuthorException;
 import com.fasttime.domain.member.exception.UserNotFoundException;
+import com.fasttime.domain.member.exception.UserNotMatchInfoException;
+import com.fasttime.domain.member.exception.UserNotMatchRePasswordException;
+import com.fasttime.domain.member.exception.UserSoftDeletedException;
 import com.fasttime.global.util.ResponseDTO;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+@Slf4j
 @RestControllerAdvice(basePackages = "com.fasttime.domain.member")
 public class MemberControllerAdvice {
 
-    @ExceptionHandler   // 비밀번호가 틀리거나 비밀번호 재확인이 일치하지않는 예외
+    @ExceptionHandler
     public ResponseEntity<ResponseDTO<Object>> badCredentialsException(BadCredentialsException e) {
         Map<String, Object> message = new HashMap<>();
         message.put("error", e.getMessage());
@@ -22,10 +27,31 @@ public class MemberControllerAdvice {
             .contentType(MediaType.APPLICATION_JSON)
             .body(ResponseDTO.res(HttpStatus.BAD_REQUEST, message));
     }
-    @ExceptionHandler //등록되지 않는 이메일로 접근시 발생하는 예외
+    @ExceptionHandler
     public ResponseEntity<ResponseDTO> userNotFoundException(UserNotFoundException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
             ResponseDTO.res(HttpStatus.BAD_REQUEST,
                 e.getMessage()));
     }
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Object>> userNotMatchInfoException(UserNotMatchInfoException e){
+        log.error("userNotMatchInfoException: ",e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(ResponseDTO.res(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage()));
+    }
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Object>> userSoftDeletedException(UserSoftDeletedException e){
+        log.error("userSoftDeletedException: ",e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(ResponseDTO.res(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage()));
+    }
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Object>> userNotMatchRePasswordException(
+        UserNotMatchRePasswordException e){
+        log.error("userNotMatchRePasswordException: ",e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(ResponseDTO.res(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage()));
+    }
 }
+
+
