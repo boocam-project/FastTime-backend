dff6457ac8b7be63e891896d5f16389740e0f85c
seungju.kim <pipiru100@gmail.com>
2023-11-22 16:37:29 +0900

    add develop ci/cd

--- src/main/java/com/fasttime/global/config/ApplicationEnvConfig.java
+++ src/main/java/com/fasttime/global/config/ApplicationEnvConfig.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Deprecated(since = "2023-11-22", forRemoval = false)
 public class ApplicationEnvConfig {
+
     private final Environment env;
 
     @Autowired
