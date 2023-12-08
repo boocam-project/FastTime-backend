3d744f94767ff09d2c78ab0acc88e8f10fe74463
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-12-08 23:52:34 +0900

    [feat] SpringSecurityConfig 구현

--- src/main/java/com/fasttime/global/config/SpringSecurityConfig.java
+++ src/main/java/com/fasttime/global/config/SpringSecurityConfig.java
@@ -1,61 +1,71 @@
 package com.fasttime.global.config;
 
+import com.fasttime.global.jwt.JwtAccessDeniedHandler;
+import com.fasttime.global.jwt.JwtAuthenticationEntryPoint;
+import com.fasttime.global.jwt.JwtAuthenticationFilter;
+import com.fasttime.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 /**
  * Security 설정 Config
  */
-
-//    private final JwtProvider jwtProvider;
-//    private final AccessDeniedHandlerImpl accessDeniedHandler;
-
-public class SpringSecurityConfig {
-    //    private final JwtProvider jwtProvider;
-//    private final AccessDeniedHandlerImpl accessDeniedHandler;
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig implements WebMvcConfigurer {
+    private final JwtProvider jwtProvider;
+    private final CorsConfig corsConfig;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private static final String[] PERMIT_URL_ARRAY = {
+        "/",
+        "/error",
+        "/docs/**",
+        "/api/v1/join",
+        "/api/v2/login",
+        "/api/v2/refresh",
+        "/api/v1/admin/join"
+    };
 
     @Bean
-    SecurityFilterChain http(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable)
-            .csrf(AbstractHttpConfigurer::disable);
-
+            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers(PERMIT_URL_ARRAY).permitAll()
+                .requestMatchers(HttpMethod.GET,"api/v1/article").permitAll()
+                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .formLogin(AbstractHttpConfigurer::disable)
+            .addFilter(corsConfig.corsFilter())
+            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
+                UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(AuthenticationManager -> AuthenticationManager
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler));
         return http.build();
     }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic().disable() // basic authentication filter 비활성화 for 보안
-//            .csrf().disable()
-//            .formLogin().disable()
-//            .rememberMe().disable()
-//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            .and().authorizeRequests()
-//            .antMatchers("/api/v1/join", "/api/v1/login", "/api/v1/admin/join", "/error")
-//            .permitAll()
-//            .antMatchers(HttpMethod.GET, "/api/v1/article").permitAll()
-//            .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
-//            .anyRequest().authenticated()
-//            .and()
-//            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
-//                UsernamePasswordAuthenticationFilter.class)
-//            .exceptionHandling()
-//            .accessDeniedHandler(accessDeniedHandler);
-//
-//        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/logout"));
-//    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-
 
 }
