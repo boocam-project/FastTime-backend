993710891808bf0d1f10ed9beccaa88c4ebf280b
seungju.kim <pipiru100@gmail.com>
2023-12-10 19:58:15 +0900

    :sparkles: ThreadConfigTest 추가
    ThreadConfig 클래스에서 AsyncTaskExecutor의 올바른 구성을 보장하기 위해 새로운 테스트 클래스 ThreadConfigTest 생성.
    테스트에서는 taskExecutor가 null이 아니며 가상 스레드에서 작업을 실행하는지 확인합니다.
    
    Signed-off-by: seungju.kim <pipiru100@gmail.com>

--- src/test/java/com/fasttime/global/config/ThreadConfigTest.java
+++ src/test/java/com/fasttime/global/config/ThreadConfigTest.java
@@ -1,23 +1,17 @@
 package com.fasttime.global.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-import org.springframework.test.context.ActiveProfiles;
-
-import java.util.concurrent.Executors;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ContextConfiguration(classes = ThreadConfig.class)
