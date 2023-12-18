8e1bc42785ffcb86fbea9a76b62643ee80d9d6a6
hym <ghrltjdtprbs@gmail.com>
2023-12-18 23:03:17 +0900

    [feat] Tag 엔터티 추가
    변경 사항 :
     - Tag 엔터티 추가

---
+++ src/main/java/com/fasttime/domain/review/entity/Tag.java
@@ -0,0 +1,25 @@
+package com.fasttime.domain.review.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.util.Set;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Review> reviews;
+
+
+    private String content;
+
+}
