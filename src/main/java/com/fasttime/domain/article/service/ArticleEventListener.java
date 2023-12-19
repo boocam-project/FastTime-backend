f21667629302f9eb9ddb96101bc90cac6229b1b2
Nine-JH <rnwkgus2@gmail.com>
2023-12-19 11:12:03 +0900

    [feat] : Article CommentCount Update Event 생성
    motivation:
    - Article 에서 조회의 간편성을 위해 Comment에 @OneToMany 연관관계를 설정하였습니다. 하지만 이로 인해 쿼리 최적화와 유지보수에 어려움이 있어 연관관계를 끊어버리고 CommentCount column을 갖도록 변경하였습니다.
    
    modification:
    - Article Entity 에서 List<Comment> 제거.
    - Article의 CommentCount를 업데이트하는 Event 생성.

---
+++ src/main/java/com/fasttime/domain/article/service/ArticleEventListener.java
@@ -0,0 +1,38 @@
+package com.fasttime.domain.article.service;
+
+import com.fasttime.domain.article.entity.Article;
+import com.fasttime.domain.article.exception.ArticleNotFoundException;
+import com.fasttime.domain.article.repository.ArticleRepository;
+import com.fasttime.domain.comment.infra.CommentCreateEvent;
+import com.fasttime.domain.comment.infra.CommentDeleteEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Component
+public class ArticleEventListener {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleEventListener(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @EventListener
+    public void updateCommentCount(CommentCreateEvent event) {
+        Article article = articleRepository.findById(event.articleId())
+            .orElseThrow(() -> new ArticleNotFoundException(event.articleId()));
+
+        article.increaseCommentCount();
+    }
+
+    @EventListener
+    public void updateCommentCount(CommentDeleteEvent event) {
+        Article article = articleRepository.findById(event.articleId())
+            .orElseThrow(() -> new ArticleNotFoundException(event.articleId()));
+
+        article.decreaseCommentCount();
+    }
+}
