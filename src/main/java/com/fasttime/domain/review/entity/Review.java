fe9d8611251e528792b1d3ef0aad7830152e4c7e
hym <ghrltjdtprbs@gmail.com>
2023-12-19 14:57:47 +0900

    [feat] 중간 테이블 ReviewTag 생성
    문제 사항 :
     - ManyToMany 사용 대신 중간 테이블 활용을 피드백 받음.
    변경 사항 :
     - 중간 테이블 ReviewTag 생성
     - ManyToMany 삭제 및 수정

--- src/main/java/com/fasttime/domain/review/entity/Review.java
+++ src/main/java/com/fasttime/domain/review/entity/Review.java
@@ -1,6 +1,5 @@
 package com.fasttime.domain.review.entity;
 
-
 import com.fasttime.domain.member.entity.Member;
 import com.fasttime.global.common.BaseTimeEntity;
 import jakarta.persistence.Entity;
@@ -8,18 +7,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
 public class Review extends BaseTimeEntity {
 
     @Id
@@ -31,18 +34,11 @@ public class Review extends BaseTimeEntity {
     private int rating;
     private String content;
 
-    @ManyToMany
-    @JoinTable(
-        name = "review_tag",
-        joinColumns = @JoinColumn(name = "review_id"),
-        inverseJoinColumns = @JoinColumn(name = "tag_id")
-    )
-    private Set<Tag> tags = new HashSet<>();
-
+    @OneToMany(mappedBy = "review")
+    private Set<ReviewTag> reviewTags = new HashSet<>();
 
     @JoinColumn(name = "member_id")
     @ManyToOne
     private Member member;
-
 
 }
--- src/main/java/com/fasttime/domain/review/entity/Tag.java
+++ src/main/java/com/fasttime/domain/review/entity/Tag.java
@@ -4,21 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import java.util.HashSet;
 import java.util.Set;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Getter
 public class Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToMany(mappedBy = "tags")
-    private Set<Review> reviews;
-
+    @OneToMany(mappedBy = "tag")
+    private Set<ReviewTag> reviewTags = new HashSet<>();
 
     private String content;
 
