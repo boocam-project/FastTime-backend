07e00c40587ff7133ef3a40c6bf06b4eeba83504
hym <ghrltjdtprbs@gmail.com>
2024-02-06 18:43:13 +0900

    [refactor] ReviewController url 중복 리팩토링
    문제 사항 :
     - url 중복
    변경 사항 :
     - @RequestMapping 으로 url 중복 최소화

--- src/main/java/com/fasttime/domain/review/controller/ReviewController.java
+++ src/main/java/com/fasttime/domain/review/controller/ReviewController.java
@@ -14,16 +14,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v2/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
     private final SecurityUtil securityUtil;
 
-    @PostMapping("api/v2/reviews")
+    @PostMapping
     public ResponseEntity<ResponseDTO<ReviewResponseDTO>> createReview(@RequestBody ReviewRequestDTO requestDTO) {
         Long memberId = securityUtil.getCurrentMemberId();
         Review review = reviewService.createReview(requestDTO, memberId);
@@ -37,14 +39,14 @@ public class ReviewController {
         return ResponseEntity.ok(ResponseDTO.res(HttpStatus.CREATED,"리뷰작성이 완료되었습니다.", responseDTO));
     }
 
-    @DeleteMapping("api/v2/reviews/{reviewId}")
+    @DeleteMapping("/{reviewId}")
     public ResponseEntity<ResponseDTO<?>> deleteReview(@PathVariable Long reviewId) {
         Long memberId = securityUtil.getCurrentMemberId();
         reviewService.deleteReview(reviewId, memberId);
         return ResponseEntity.ok(ResponseDTO.res(HttpStatus.OK, "리뷰삭제가 완료되었습니다."));
     }
 
-    @PutMapping("api/v2/reviews/{reviewId}")
+    @PutMapping("/{reviewId}")
     public ResponseEntity<ResponseDTO<ReviewResponseDTO>> updateReview(
         @PathVariable Long reviewId,
         @RequestBody ReviewRequestDTO requestDTO) {
