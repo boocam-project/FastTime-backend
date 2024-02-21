b9adbb491669617fa0278d1a23bddc97958a9312
JeongUijeong <jeong275117@gmail.com>
2024-02-21 22:09:33 +0900

    [feat] 공모전 조회 메서드 추가

--- src/main/java/com/fasttime/domain/reference/repository/CompetitionRepository.java
+++ src/main/java/com/fasttime/domain/reference/repository/CompetitionRepository.java
@@ -1,8 +1,14 @@
 package com.fasttime.domain.reference.repository;
 
 import com.fasttime.domain.reference.entity.Competition;
+import com.fasttime.domain.reference.entity.RecruitmentStatus;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
 
+    List<Competition> findAllByStatus(RecruitmentStatus status);
+
+    Optional<Competition> findByTitle(String title);
 }
