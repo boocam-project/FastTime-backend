8eed6cdb4d4ddd32516c99f38d2cd5e55f661a3f
JeongUijeong <jeong275117@gmail.com>
2024-02-21 21:11:45 +0900

    [feat] 대외활동, 공모전 Entity 구현

---
+++ src/main/java/com/fasttime/domain/reference/entity/Activity.java
@@ -0,0 +1,101 @@
+package com.fasttime.domain.reference.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String organization;
+
+    private String corporateType;
+
+    private String participate;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private String period;
+
+    private int recruitment;
+
+    private String area;
+
+    private String preferredSkill;
+
+    private String homepageUrl;
+
+    private String field;
+
+    private String activityBenefit;
+
+    private String bonusBenefit;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private RecruitmentStatus status;
+
+    @Builder
+    private Activity(
+        Long id,
+        String title,
+        String organization,
+        String corporateType,
+        String participate,
+        LocalDate startDate,
+        LocalDate endDate,
+        String period,
+        int recruitment,
+        String area,
+        String preferredSkill,
+        String homepageUrl,
+        String field,
+        String activityBenefit,
+        String bonusBenefit,
+        String description,
+        String imageUrl,
+        RecruitmentStatus status
+    ) {
+        this.id = id;
+        this.title = title;
+        this.organization = organization;
+        this.corporateType = corporateType;
+        this.participate = participate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.period = period;
+        this.recruitment = recruitment;
+        this.area = area;
+        this.preferredSkill = preferredSkill;
+        this.homepageUrl = homepageUrl;
+        this.field = field;
+        this.activityBenefit = activityBenefit;
+        this.bonusBenefit = bonusBenefit;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/fasttime/domain/reference/entity/Competition.java
@@ -0,0 +1,74 @@
+package com.fasttime.domain.reference.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Competition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String organization;
+
+    private String corporateType;
+
+    private String participate;
+
+    private int awardScale;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private String homepageUrl;
+
+    private String activityBenefit;
+
+    private String bonusBenefit;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private RecruitmentStatus status;
+
+    @Builder
+    private Competition(Long id, String title, String organization, String corporateType,
+        String participate, int awardScale, LocalDate startDate, LocalDate endDate,
+        String homepageUrl,
+        String activityBenefit, String bonusBenefit, String description, String imageUrl,
+        RecruitmentStatus status) {
+        this.id = id;
+        this.title = title;
+        this.organization = organization;
+        this.corporateType = corporateType;
+        this.participate = participate;
+        this.awardScale = awardScale;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.homepageUrl = homepageUrl;
+        this.activityBenefit = activityBenefit;
+        this.bonusBenefit = bonusBenefit;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.status = status;
+    }
+}
