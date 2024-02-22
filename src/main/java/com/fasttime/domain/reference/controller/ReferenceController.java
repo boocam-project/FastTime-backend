d2e2e3c6671a4cc92e259a53e7cb0554fa45665d
JeongUijeong <jeong275117@gmail.com>
2024-02-22 20:21:38 +0900

    [feat] 대외활동 상세 조회 API 컨트롤러 구현

--- src/main/java/com/fasttime/domain/reference/controller/ReferenceController.java
+++ src/main/java/com/fasttime/domain/reference/controller/ReferenceController.java
@@ -3,6 +3,7 @@ package com.fasttime.domain.reference.controller;
 import com.fasttime.domain.reference.dto.request.ReferencePageRequestDto;
 import com.fasttime.domain.reference.dto.request.ReferenceSearchRequestDto;
 import com.fasttime.domain.reference.dto.response.ActivityPageResponseDto;
+import com.fasttime.domain.reference.dto.response.ActivityResponseDto;
 import com.fasttime.domain.reference.dto.response.CompetitionPageResponseDto;
 import com.fasttime.domain.reference.service.usecase.ReferenceServiceUseCase;
 import com.fasttime.global.util.ResponseDTO;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,6 +77,17 @@ public class ReferenceController {
                         .pageSize(pageSize)
                         .build().toPageable()
                 )
+            )
+        );
+    }
+
+    @GetMapping("/activities/{activityId}")
+    public ResponseEntity<ResponseDTO<ActivityResponseDto>> searchActivities(
+        @PathVariable(name = "activityId") int activityId) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDTO.res(HttpStatus.OK,
+                "대외활동 상세 정보를 성공적으로 조회했습니다.",
+                referenceServiceUseCase.getActivity(activityId)
             )
         );
     }
