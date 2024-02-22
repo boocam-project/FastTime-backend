344d4954d96e4bdbc3d64867b9970a61e1b52879
JeongUijeong <jeong275117@gmail.com>
2024-02-22 17:19:55 +0900

    [refactor] 대외활동, 공모전 응답 DTO 수정

--- src/main/java/com/fasttime/domain/reference/dto/response/ReferenceResponseDto.java
+++ src/main/java/com/fasttime/domain/reference/dto/response/ReferenceResponseDto.java
@@ -1,6 +1,7 @@
 package com.fasttime.domain.reference.dto.response;
 
 import com.fasttime.domain.reference.entity.Activity;
+import com.fasttime.domain.reference.entity.Competition;
 import lombok.Builder;
 
 @Builder
@@ -8,12 +9,24 @@ public record ReferenceResponseDto(
     long id,
     String title,
     String organization,
-    long dDay,
     String imageUrl
 ) {
 
     public static ReferenceResponseDto of(Activity activity) {
         return ReferenceResponseDto.builder()
+            .id(activity.getId())
+            .title(activity.getTitle())
+            .organization(activity.getOrganization())
+            .imageUrl(activity.getImageUrl())
+            .build();
+    }
+
+    public static ReferenceResponseDto of(Competition competition) {
+        return ReferenceResponseDto.builder()
+            .id(competition.getId())
+            .title(competition.getTitle())
+            .organization(competition.getOrganization())
+            .imageUrl(competition.getImageUrl())
             .build();
     }
 }
