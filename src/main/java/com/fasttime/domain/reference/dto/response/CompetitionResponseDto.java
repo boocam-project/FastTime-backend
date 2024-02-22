87b0782636fa429ec6075a3e02bdbf9d7d02c3d8
JeongUijeong <jeong275117@gmail.com>
2024-02-22 20:30:11 +0900

    [feat] 공모전 응답 DTO 구현

---
+++ src/main/java/com/fasttime/domain/reference/dto/response/CompetitionResponseDto.java
@@ -0,0 +1,42 @@
+package com.fasttime.domain.reference.dto.response;
+
+import com.fasttime.domain.reference.entity.Activity;
+import com.fasttime.domain.reference.entity.Competition;
+import java.time.LocalDate;
+import lombok.Builder;
+
+@Builder
+public record CompetitionResponseDto(
+    long id,
+    String title,
+    String organization,
+    String corporateType,
+    String participate,
+    String awardScale,
+    LocalDate startDate,
+    String endDate,
+    String homepageUrl,
+    String activityBenefit,
+    String bonusBenefit,
+    String description,
+    String imageUrl
+) {
+
+    public static CompetitionResponseDto of(Competition competition) {
+        return CompetitionResponseDto.builder()
+            .id(competition.getId())
+            .title(competition.getTitle())
+            .organization(competition.getOrganization())
+            .corporateType(competition.getCorporateType())
+            .participate(competition.getParticipate())
+            .awardScale(competition.getAwardScale())
+            .startDate(competition.getStartDate())
+            .endDate(competition.getEndDate())
+            .homepageUrl(competition.getHomepageUrl())
+            .activityBenefit(competition.getActivityBenefit())
+            .bonusBenefit(competition.getBonusBenefit())
+            .description(competition.getDescription())
+            .imageUrl(competition.getImageUrl())
+            .build();
+    }
+}
