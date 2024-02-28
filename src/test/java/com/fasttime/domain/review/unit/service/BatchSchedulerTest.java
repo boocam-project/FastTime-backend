b8e2ae0b18f3c201c544735e5d96d901d2f00d78
hym <ghrltjdtprbs@gmail.com>
2024-02-28 19:41:57 +0900

    [feat] 배치작업 스케줄러 테스트 코드 추가
    문제 사항 :
     - 매일 새벽 3시에 실행되는 DeleteOldReviewsJob의 정상 작동 검증 필요
    변경 사항 :
     - BatchSchedulerTest 추가
     - JobLauncher의 run 메서드가
       올바르게 호출되는지 검증.

---
+++ src/test/java/com/fasttime/domain/review/unit/service/BatchSchedulerTest.java
@@ -0,0 +1,40 @@
+package com.fasttime.domain.review.unit.service;
+
+import com.fasttime.domain.review.service.BatchScheduler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+
+import static org.mockito.Mockito.*;
+
+class BatchSchedulerTest {
+
+    @Mock
+    private JobLauncher jobLauncher;
+    @Mock
+    private Job deleteOldReviewsJob;
+
+    @InjectMocks
+    private BatchScheduler batchScheduler;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void runDeleteOldReviewsJob() throws Exception {
+        when(jobLauncher.run(deleteOldReviewsJob, new JobParameters())).thenReturn(
+            new JobExecution(0L));
+
+        batchScheduler.runDeleteOldReviewsJob();
+
+        verify(jobLauncher).run(deleteOldReviewsJob, new JobParameters());
+    }
+}
