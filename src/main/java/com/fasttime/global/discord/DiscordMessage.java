3a9a4da9f5fa94b91349df05f0317abe37ba340d
seungju.kim <pipiru100@gmail.com>
2024-03-01 17:49:03 +0900

    Exception Log 수정
    Signed-off-by: seungju.kim <pipiru100@gmail.com>

--- src/main/java/com/fasttime/global/discord/DiscordMessage.java
+++ src/main/java/com/fasttime/global/discord/DiscordMessage.java
@@ -2,7 +2,7 @@ package com.fasttime.global.discord;
 
 public record DiscordMessage(String content) {
 
-  public static DiscordMessage createDiscordMessage(String message) {
+  public static DiscordMessage createErrorDiscordMessage(String message) {
     return new DiscordMessage(message);
   }
 }
--- src/main/java/com/fasttime/global/discord/ExceptionAlertAspect.java
+++ src/main/java/com/fasttime/global/discord/ExceptionAlertAspect.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +15,10 @@ import org.springframework.stereotype.Component;
 public class ExceptionAlertAspect {
   private final DiscordFeignClient discordFeignClient;
 
+  @Value("${spring.profiles.active}")
+  private String serverValue;
   public void sendAlert(String message) {
-    discordFeignClient.sendMessage(DiscordMessage.createDiscordMessage(message));
+    discordFeignClient.sendMessage(DiscordMessage.createErrorDiscordMessage(message));
   }
 
   @AfterThrowing(
@@ -23,10 +26,10 @@ public class ExceptionAlertAspect {
           "within(@org.springframework.web.bind.annotation.RestController *)",
       throwing = "e")
   public void sendExceptionAlert(JoinPoint joinPoint, Exception e) {
-    log.error("예외 발생!", e);
     String alertMessage =
         String.format(
-            "예외 발생! 클래스: %s, 메서드: %s, 메시지: %s",
+            "server-profile = `%s` 예외 발생! \n 클래스:` %s ` \n 메서드:` %s ` \n 메시지:` %s `",
+            serverValue,
             joinPoint.getSignature().getDeclaringTypeName(),
             joinPoint.getSignature().getName(),
             e.getMessage());
