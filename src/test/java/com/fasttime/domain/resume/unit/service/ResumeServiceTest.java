ee7759f6df13613d4feed01550469b8636823298
Kongji82 <Kongji4092@gmail.com>
2024-03-16 20:28:53 +0900

    [test] updateResume() 에러 테스트코드 작성
    - 자기소개서 작성자가 아닌 경우 NoResumeWriterException

--- src/test/java/com/fasttime/domain/resume/unit/service/ResumeServiceTest.java
+++ src/test/java/com/fasttime/domain/resume/unit/service/ResumeServiceTest.java
@@ -1,16 +1,22 @@
 package com.fasttime.domain.resume.unit.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import com.fasttime.domain.member.entity.Member;
 import com.fasttime.domain.member.service.MemberService;
 import com.fasttime.domain.resume.dto.ResumeRequestDto;
 import com.fasttime.domain.resume.dto.ResumeResponseDto;
+import com.fasttime.domain.resume.dto.ResumeUpdateServiceRequest;
 import com.fasttime.domain.resume.entity.Resume;
+import com.fasttime.domain.resume.exception.NoResumeWriterException;
+import com.fasttime.domain.resume.exception.ResumeNotFoundException;
 import com.fasttime.domain.resume.repository.ResumeRepository;
 import com.fasttime.domain.resume.service.ResumeService;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -62,18 +68,19 @@ class ResumeServiceTest {
 
     @DisplayName("updateResume()는")
     @Nested
-    class Context_updateResume{
+    class Context_updateResume {
 
         @DisplayName("자기소개서를 성공적으로 편집한다.")
         @Test
-        void _willSuccess(){
+        void _willSuccess() {
             // given
             Member member = Member.builder().id(1L).nickname("testName").build();
             Resume mockResume = createMockResume(member);
 
             String updatedTitle = "updated title";
             String updatedContent = "updated content";
-            ResumeUpdateServiceRequest updateRequest = new ResumeUpdateServiceRequest(MOCK_RESUME_ID, 1L,
+            ResumeUpdateServiceRequest updateRequest = new ResumeUpdateServiceRequest(
+                    MOCK_RESUME_ID, 1L,
                     updatedTitle, updatedContent);
 
             given(memberService.getMember(1L)).willReturn(member);
@@ -85,6 +92,38 @@ class ResumeServiceTest {
             assertThat(response).extracting("id", "title", "content", "writer", "rating")
                     .containsExactly(1L, updatedTitle, updatedContent, "testName", 0);
         }
+
+        @DisplayName("자기소개서 작성자가 아닌 경우 NoResumeWriterException을 반환한다.")
+        @Test
+        void resume_validateFail_throwIllegalArgumentException() {
+            Member writer = Member.builder().id(1L).nickname("testName").build();
+            Member notAuthorizedMember = Member.builder().id(221L).build();
+            Resume mockResume = createMockResume(writer);
+            ResumeUpdateServiceRequest updateRequest = new ResumeUpdateServiceRequest(
+                    MOCK_RESUME_ID, notAuthorizedMember.getId(),
+                    "updateTitle", "updateContent");
+
+            given(memberService.getMember(anyLong())).willReturn(notAuthorizedMember);
+            given(resumeRepository.findById(anyLong())).willReturn(Optional.of(mockResume));
+
+            assertThatThrownBy(() -> resumeService.updateResume(updateRequest))
+                    .isInstanceOf(NoResumeWriterException.class);
+
+        }
+
+        @DisplayName("수정할 자기소개서가 없는 경우 ResumeNotFoundException을 반환한다.")
+        @Test
+        void resume_notExist_throwExceptoin() {
+            ResumeUpdateServiceRequest request = new ResumeUpdateServiceRequest(MOCK_RESUME_ID, 1L,
+                    "updateTitle", "updateContent");
+
+            given(resumeRepository.findById(anyLong())).willReturn(Optional.empty());
+
+            assertThatThrownBy(() -> resumeService.updateResume(request))
+                    .isInstanceOf(ResumeNotFoundException.class);
+        }
+
+
     }
 
     @DisplayName("deleteResume()는")
